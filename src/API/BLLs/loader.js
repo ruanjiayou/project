7235e7f71bd5cfeceab15fcf8d2b73e912054273
Adrian Sieber <mail@adriansieber.com>
2018-02-16 13:08:07 +0000

    Fix code style

--- src/API/BLLs/loader.js
+++ src/API/BLLs/loader.js
@@ -3,29 +3,32 @@ const dir = __dirname;
 const path = require('path');
 const BLLs = {};
 
-function scanner(dirname) {
-    fs.readdirSync(dirname).forEach((file)=>{
-        let fullname= path.join(dirname, file);
-        let ext = path.extname(file);
-        // auth/user.js --> AuthUser
-        let filename = fullname
-            .replace(dir, '')
-            .replace(/([.].+)$/, '')
-            .replace(/\\/g, '/')
-            .split('/')
-            .map(function(item){
-                let m = /^([a-z]).+$/.exec(item);
-                if(m){
-                    item = item.replace(m[1], m[1].toUpperCase());
-                }
-                return item;
-            })
-            .join('');
-        if(fs.existsSync(fullname) && fs.lstatSync(fullname).isDirectory()){
-            scanner(fullname);
-        } else if(fullname!==module.filename && ext.toLowerCase() === '.js'){
-            BLLs[filename] = require(fullname);
-        }
+function scanner (dirname) {
+  fs.readdirSync(dirname)
+    .forEach((file) => {
+      const fullname = path.join(dirname, file);
+      const ext = path.extname(file);
+      // auth/user.js --> AuthUser
+      const filename = fullname
+        .replace(dir, '')
+        .replace(/([.].+)$/, '')
+        .replace(/\\/g, '/')
+        .split('/')
+        .map((item) => {
+          const m = /^([a-z]).+$/.exec(item);
+          if (m) {
+            item = item.replace(m[1], m[1].toUpperCase());
+          }
+          return item;
+        })
+        .join('');
+      if (fs.existsSync(fullname) && fs.lstatSync(fullname)
+        .isDirectory()) {
+        scanner(fullname);
+      }
+      else if (fullname !== module.filename && ext.toLowerCase() === '.js') {
+        BLLs[filename] = require(fullname);
+      }
     });
 }
 scanner(dir);
