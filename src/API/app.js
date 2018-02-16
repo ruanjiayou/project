bdf07111cb4eb06dd910cfc87170022444f7ae18
ruanjiayou <1439120442@qq.com>
2018-02-16 14:22:47 +0800

    增加websocket

--- src/API/app.js
+++ src/API/app.js
@@ -5,7 +5,8 @@ global.$models = require('./models/loader');
 global.$BLLs = require('./BLLs/loader');
 
 const express = require('express');
-const Server = express();
+const app = express();
+const server = require('http').createServer(app);
 const path = require('path');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
@@ -15,35 +16,38 @@ const ejs = require('ejs');
 const cfgs = global.$cfgs;
 const libs = global.$libs;
 
+// ws消息
+require('../ws/websocket')(server);
+
 // 1.设置express模板引擎
-Server.set('views', path.join(__dirname, 'templates', 'views'));
-Server.set('view engine', 'html');
-Server.engine('.html', ejs.__express);
+app.set('views', path.join(__dirname, 'templates', 'views'));
+app.set('view engine', 'html');
+app.engine('.html', ejs.__express);
 ejs.delimiter = '$';
 // 安全部分
-Server.use(helmet());
+app.use(helmet());
 
 // 2.静态目录
-Server.use(express.static(path.join(__dirname, '../web/public')));
-Server.use(express.static('d:/website/blog.php/'));
+app.use(express.static(path.join(__dirname, '../web/public')));
+app.use(express.static('d:/website/blog.php/'));
 // 3.解析请求
-Server.use(cookieParser());
-Server.use(bodyParser.json());
-Server.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 // 国际化
-Server.use(libs.i18n);
+app.use(libs.i18n);
 // 文件处理中间件
-Server.use(libs.upload);
+app.use(libs.upload);
 // 自动清理文件...日
-//Server.use(multerAutoReap);
+//app.use(multerAutoReap);
 // 4.全局变量
-Server.use(function (req, res, next) {
+app.use(function (req, res, next) {
     res.locals.sys = cfgs.site.sys;
     next();
 });
 
 // 5.添加自定义响应方法(自动处理json:status与result)
-Server.use(libs.present({
+app.use(libs.present({
     page: 'page',
     limit: 'limit',
     search: 'search',
@@ -53,10 +57,10 @@ Server.use(libs.present({
     validateDir: path.join(__dirname, 'templates/validate-errors')
 }));
 // 6.路由 含token验证
-router(Server);
+router(app);
 
 // 7.error异常处理
-Server.use(function (err, req, res, next) {
+app.use(function (err, req, res, next) {
     if (err instanceof HinterError) {
         // 自定义错误
         res.customError(err);
@@ -71,7 +75,7 @@ Server.use(function (err, req, res, next) {
     }
 });
 // 8.404
-Server.use(function (req, res) {
+app.use(function (req, res) {
     //console.log(req.method);
     //console.log(req.originalUrl.length);
     //console.log(req.originalUrl);
@@ -82,10 +86,10 @@ Server.use(function (req, res) {
 
 
 if (module.parent) {
-    module.exports = Server;
+    module.exports = app;
 } else {
     // 监听端口，启动程序
-    Server.listen(cfgs.site.port, '0.0.0.0', function () {
+    server.listen(cfgs.site.port, '0.0.0.0', function () {
         console.log(`图片站(API/web一体) 监听端口:${cfgs.site.port}`);
     });
 }
---
+++ src/ws/websocket.js
@@ -0,0 +1,72 @@
+const wsio = require('socket.io');
+const debug = require('debug')('APP:ws');
+module.exports = function (server) {
+    const io = wsio(server);
+    // 设置日志级别
+    io.serveClient('log level', 1);
+    // websocket连接监听
+    io.on('connection', function (socket) {
+        socket.emit('open');
+        const client = {
+            socket: socket,
+            name: false,
+            color: getColor()
+        };
+        // 对message时间的监听
+        socket.on('message', function (msg) {
+            let obj = {};
+            // 判断是不是第一次连接,以第一条消息作为用户名
+            if (!client.name) {
+                client.name = msg;
+                obj = {
+                    text: msg,
+                    author: 'System',
+                    type: 'welcome',
+                    time: getTime(),
+                    color: client.color
+                };
+                debug(`${msg} login`);
+                // 返回欢迎语
+                socket.emit('system', obj);
+                // 广播新用户登录
+                socket.broadcast.emit('system', obj);
+            } else {
+                obj = {
+                    text: msg,
+                    author: client.name,
+                    type: 'message',
+                    time: getTime(),
+                    color: client.color
+                };
+                debug(`${client.name} say:${msg}`);
+                // 返回消息,可以省略
+                socket.emit('message', obj);
+                // 广播向其他用户发消息
+                socket.broadcast.emit('message', obj);
+            }
+        });
+        // 对退出事件的监听
+        socket.on('desconnect', function () {
+            let obj = {
+                time: getTime(),
+                color: client.color,
+                author: 'System',
+                text: client.name,
+                type: 'disconnect'
+            };
+            // 广播用户已退出的信息
+            socket.broadcase.emit('system', obj);
+            debug(`${client.name} Disconnect`);
+        });
+    });
+};
+
+function getTime() {
+    let d = new Date();
+    return `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+}
+
+function getColor() {
+    var colors = ['aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'pink', 'red', 'green', 'orange', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue'];
+    return colors[Math.round(Math.random() * 10000 % colors.length)];
+}
