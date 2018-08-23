8767d296a8f3ad0d262a6f4065ef6b53857d7c5f
ruanjiayou <1439120442@qq.com>
2018-08-24 00:27:10 +0800

    改进基础架构

--- config.project.js
+++ config.project.js
@@ -1,13 +1,6 @@
-// 所有项目必填参数
-define('PORT', '3000');
-define('NODE_ENV', 'dev');
-define('PROJECT_NAME', 'project');
-
+require('./config.default');
 // 项目单独参数
-
-// 环境变量
-process.env.port = global.PORT;
-process.env.NODE_ENV = global.NODE_ENV;
+define('PROJECT_NAME', 'project');
 
 // 鉴权
 define('AUTH_KEY', 'token');  // 鉴权字段
