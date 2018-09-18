18dc4cd6dfc8628012522102876246466ccaf179
ruanjiayou <1439120442@qq.com>
2018-09-18 23:34:12 +0800

    测试静态化,效果明显

--- src/BLL/ChapterBLL.js
+++ src/BLL/ChapterBLL.js
@@ -9,6 +9,16 @@ class ChapterBLL extends BaseBLL {
     this.model = this.models.Chapter;
     this.attributes = this.getAttributes();
   }
+  async getInfo(opts = {}) {
+    const opt = this._init(opts);
+    let res = await this.model.scope(opt.scopes).findOne(opt);
+    if (res) {
+      res = res.toJSON();
+      res.prev = await this.model.findOne({ order: [['id', 'DESC']], attributes: ['id', 'bookId', 'title'], where: { bookId: res.bookId, id: { [this.models.Op.lt]: res.id } } });
+      res.next = await this.model.findOne({ attributes: ['id', 'bookId', 'title'], where: { bookId: res.bookId, id: { [this.models.Op.gt]: res.id } } });
+    }
+    return res;
+  }
 }
 
 module.exports = ChapterBLL;
--- src/routes/public/chapter.js
+++ src/routes/public/chapter.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const ioHelper = require(LIB_PATH + '/ioHelper');
 const ChapterBLL = require(BLL_PATH + '/ChapterBLL');
 const chapterBLL = new ChapterBLL();
 
@@ -8,21 +9,31 @@ module.exports = {
    * @apiGroup public-book-chapter
    */
   'get /v2/public/book/:bookId([0-9]+)/chapters': async (req, res, next) => {
-    const hql = req.paging((q) => {
-      q.limit = 0;
-      q.where['bookId'] = req.params.bookId;
-      q.attributes = 'id,bookId,title,order,createdAt,updatedAt';
-      return q;
-    });
-    const result = await chapterBLL.getList(hql);
-    return res.paging(result, hql);
+    let result = null;
+    const cachePath = `${CACHE_PATH}/books/${req.params.bookId}.json`;
+    if (ioHelper.isFileExists(cachePath)) {
+      result = JSON.parse(ioHelper.readTxt(cachePath));//17
+    } else {
+      result = await chapterBLL.getList({ limit: 0, where: { bookId: req.params.bookId }, attributes: 'id,bookId,title' });//266
+      result = _.isArray(result) ? result : (result.rows ? result.rows.map(function (item) { return item.toJSON ? item.toJSON() : item; }) : []);
+      ioHelper.writeTxt(cachePath, JSON.stringify(result));
+    }
+    return res.return(result);
   },
   /**
    * @api {get} /v2/public/book/:bookId([0-9]+)/chapter/:chapterId([0-9]+) 章节详情
    * @apiGroup public-book-chapter
    */
   'get /v2/public/book/:bookId([0-9]+)/chapter/:chapterId([0-9]+)': async (req, res, next) => {
-    const result = await chapterBLL.getInfo({ where: req.params.chapterId });
+    let result = null;
+    const cachePath = `${CACHE_PATH}/chapters/${req.params.bookId}/${req.params.chapterId}.json`;
+    if (ioHelper.isFileExists(cachePath)) {
+      result = JSON.parse(ioHelper.readTxt(cachePath));
+    } else {
+      ioHelper.mkdirs(`${CACHE_PATH}/chapters/${req.params.bookId}`);
+      result = await chapterBLL.getInfo({ where: req.params.chapterId });
+      ioHelper.writeTxt(cachePath, JSON.stringify(result));
+    }
     return res.return(result);
   }
 };
